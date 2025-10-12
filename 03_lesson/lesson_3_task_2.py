78f5178855f8c77d340cd6c6efdb15e2fb93dcc7
mike985-майк 985 <abuhtiyarov2017@yandex.ru>
2025-10-12 15:46:23 +0300

    Добавлены файлы для lesson3

---
+++ 03_lesson/lesson_3_task_2.py
@@ -0,0 +1,12 @@
+from smartphone import Smartphone
+
+catalog = [
+    Smartphone("Samsung", "Galaxy S21", "+79242718265"),
+    Smartphone("Samsung", "Galaxy S22", "+79242718264"),
+    Smartphone("Samsung", "Galaxy S23", "+79242718263"),
+    Smartphone("Samsung", "Galaxy S24", "+79242718262"),
+    Smartphone("Samsung", "Galaxy S25", "+79242718261")
+]
+
+for smartphone in catalog:
+    print(f"{smartphone.phone_brand} - {smartphone.phone_model} - {smartphone.subscription_number}")
